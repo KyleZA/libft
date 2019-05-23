e970253090d83b47a6954c4752e7881360dd8444
Kyle Francis <kfrancis@c3r8s2.wethinkcode.co.za>
2019-05-23 13:04:04 +0200

    pushing again with isalpha added, changes later

--- libc_functions_part1/isalpha.c
+++ libc_functions_part1/isalpha.c
@@ -1,29 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strncpy.c                                          :+:      :+:    :+:   */
+/*   isalpha.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/22 10:29:00 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/05/23 06:39:44 by kfrancis         ###   ########.fr       */
+/*   Created: 2019/05/23 11:43:12 by kfrancis          #+#    #+#             */
+/*   Updated: 2019/05/23 11:45:39 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char *ft_strncpy(char *dst, const char *src, size_t len)
+int isalpha(int c)
 {
-	size_t i;
-
-	while(src != '\0')
-		{
-			dst[i] = src[i];
-				len++;
-		}
-	if(src[i] > '\0')
-		{
-			dst[i] = '\0'
-		}
-	return(*dst);
+	if (c >= 101 && c <= 172)
+		
+			while(c != '\0')
+				{
+					c++;
+				}
+	else 
+			{
+				return(0);
+			}
+	return(c);
 }
