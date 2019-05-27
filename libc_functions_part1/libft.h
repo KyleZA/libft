0299c99ba948c114f84c6a886f87563b37017963
Kyle Francis <kfrancis@c3r8s2.wethinkcode.co.za>
2019-05-27 15:00:25 +0200

    libft header being pushed making changes later

---
+++ libc_functions_part1/libft.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/27 14:23:54 by kfrancis          #+#    #+#             */
+/*   Updated: 2019/05/27 14:27:06 by kfrancis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_h
+#define LIBFT_h
+
+#include <unstd.h>
+#include <string.h>
+#include <stdlib.h>
+
+//memset
+//bzero
+//memcpy
+//memccpy
+//memmove
+//memchr
+//strdup
+//strcpy
+//strncpy
+//strcat
+//strncat
+//strlcat
+//strchr
+//strrchr
+//strstr
+//strnstr
+//strcmp
+//strncmp
+//atoi (still need to complete this one)
