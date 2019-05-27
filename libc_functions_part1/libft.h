b1a48ddd0954d2bf6ac677c633b440cf6efd9941
Kyle Francis <kfrancis@c3r8s2.wethinkcode.co.za>
2019-05-27 15:12:14 +0200

    always check libft to make changes

--- libc_functions_part1/libft.h
+++ libc_functions_part1/libft.h
@@ -6,7 +6,7 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 14:23:54 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/05/27 14:27:06 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/05/27 15:11:49 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,16 @@
 //memccpy
 //memmove
 //memchr
-//strdup
+//strdup (double check the push)
 //strcpy
 //strncpy
-//strcat
+//strcat (double check the push)
 //strncat
 //strlcat
 //strchr
 //strrchr
 //strstr
 //strnstr
-//strcmp
+//strcmp (double check the push)
 //strncmp
 //atoi (still need to complete this one)
