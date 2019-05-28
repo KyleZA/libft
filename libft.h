ffe2c51e9bde6096937badbd53ed6880387e22ad
Kyle Francis <kfrancis@c3r8s2.wethinkcode.co.za>
2019-05-28 09:26:48 +0200

    added notes to header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 08:06:38 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/05/28 09:14:19 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/05/28 09:26:25 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ void *ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
 void *ft_memmove(void *dst, const void *src, size_t len);
 void *ft_memchar(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t);
-size_t ft_strlen(const char *s);
-char *ft_strdup(const char *s1);
-char *ft_strcpy(char *dst, const char *src);
-char *ft_strncpy(char *dst, const char *src, size_t len);
+size_t ft_strlen(const char *s); //double check
+char *ft_strdup(const char *s1); //double check
+char *ft_strcpy(char *dst, const char *src); //double check
+char *ft_strncpy(char *dst, const char *src, size_t len); //double check
 char *ft_strcat(char *restrict s1, const char *restrict s2);
 char *ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
 size_t ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize);
@@ -35,16 +35,16 @@ char *ft_strchr(const char *s, int c);
 char *ft_strrchr(const char *s, int c);
 char *ft_strstr(const char *haystack, const char *needle);
 char *ft_strnstr(const char *haystack, const char *needle, size_t len);
-int ft_strcmp(const char *s1, const char *s2);
-int ft_strncmp(const char *s1, const char *s2, size_t n);
-int ft_atoi(const char *str);
-int ft_isalpha(int c);
-int ft_isdigit(int c);
-int ft_isalnum(int c);
-int ft_isascii(int c);
-int ft_isprint(int c);
-int ft_toupper(int c);
-int ft_tolower(int c);
+int ft_strcmp(const char *s1, const char *s2); // double check
+int ft_strncmp(const char *s1, const char *s2, size_t n); 
+int ft_atoi(const char *str); // need to update
+int ft_isalpha(int c); //done
+int ft_isdigit(int c); //done
+int ft_isalnum(int c); //done
+int ft_isascii(int c); //done
+int ft_isprint(int c); //done
+int ft_toupper(int c); //done
+int ft_tolower(int c); //done
 
 /*additional functions*/
 
