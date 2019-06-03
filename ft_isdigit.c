a3a2a7790a94e26192bb951cffea99207b9f9386
Kyle Francis <kfrancis@c3r7s2.wethinkcode.co.za>
2019-06-03 15:38:40 +0200

    pushing tested isdigit

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -5,19 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/24 10:00:23 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/05/24 10:20:12 by kfrancis         ###   ########.fr       */
+/*   Created: 2019/06/03 15:24:52 by kfrancis          #+#    #+#             */
+/*   Updated: 2019/06/03 15:28:01 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int ft_isdigit(int c)
 {
-	if(c >= 48 && c <= 57)
-	{
-		return(1);
-	}
-	else
-	{
-		return(0);
-	}
+	if(c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }
