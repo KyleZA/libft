058529c6a1ba155f2b4bd8af4dd2b6e6a549b1bb
Kyle Francis <kfrancis@c3r8s2.wethinkcode.co.za>
2019-06-05 06:46:12 +0200

    went through all tests, strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,29 +6,30 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 15:02:28 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/05/27 15:06:43 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/05/31 09:56:35 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char * ft_strdup(const char *src)
+char	*ft_strdup(const char *s1)
 {
+	char *dst;
+
 	int i;
 
 	i = 0;
 
-		dst = (char*)malloc(ft_strlen(src)*sizeof((char) + 1);
-			if(dst == NULL)
-			{
-			return (NULL);
-			}
-		while(src[i] != '\0')
+	dst = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
+	if (dst == NULL)
+		return (NULL);
+
+		while (s1[i] != '\0')
 		{
-			dst[i] = src[i];
-			i++
+			dst[i] = s1[i];
+			i++;
 		}
-	dst[i] = '\0'
-	return (dst);
+		dst[i] = '\0';
+		return (dst);
 }
