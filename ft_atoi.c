934d58d17f6dbcf40e7455c4a45240ac56609c73
Kyle Francis <kfrancis@c3r8s2.wethinkcode.co.za>
2019-06-05 06:52:00 +0200

    not working at all

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 09:58:18 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/05/28 13:21:52 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/05/31 06:58:53 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <string.h>
@@ -27,10 +27,5 @@ int ft_atoi(const char *str)
 				return(*str);
 			}
 	}
-}
-int main()
-{
-	printf("%d\n", ft_atoi("22b3" "123" "123123"));
-	printf("%d", atoi("12b3"));
-	return(0);
+	return (0);
 }
