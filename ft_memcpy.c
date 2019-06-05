1a3113e80a482da7bc25e785d4f00fd319ae8d0f
Kyle Francis <kfrancis@c3r7s4.wethinkcode.co.za>
2019-06-05 13:31:00 +0200

    tested and completed

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,27 +6,28 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 12:41:48 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/05 12:50:59 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/05 13:22:06 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *memcpy(void *restrict dst, const void *restrict src, size_t n)
+void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
+		char *chrdst;
+		char *chrsrc;
 
-	int i;
+		chrdst = (char *)dst;
+		chrsrc = (char *)src;
 
-	i = 0;
-
-	while ((i < len) && (src != '\0'))
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	while (i < len)
-	{
-		dst[i] = NULL;
-		i++;
-	}
+		if (dst == NULL && src == NULL)
+		{
+			return (NULL);
+		}
+		while (n > 0)
+		{
+			*chrdst++ = *chrsrc++;
+			n--;
+		}
+		return (dst);
 }
