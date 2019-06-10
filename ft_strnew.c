a588cb75eb7ead72941e0a6c94d1f0eada856033
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-10 12:38:00 +0200

    commiting a completed and tested strnew

---
+++ ft_strnew.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/10 12:24:09 by kfrancis          #+#    #+#             */
+/*   Updated: 2019/06/10 12:34:02 by kfrancis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	char *str;
+	size_t i;
+
+	str = (char *)malloc(sizeof(char) * (size + 1));
+	if (!str)
+			return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		str[i] = '\0';
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
