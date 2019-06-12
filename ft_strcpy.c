19e50e238bc15dbeb565c74ae88b3f2f2963eec5
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-12 09:42:20 +0200

    completed strcpy.c

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,24 +6,26 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 10:18:07 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/11 09:54:17 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/12 09:42:05 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char ft_strcpy(char *dst, const char *src)
+char *ft_strcpy(char *dst, const char *src)
 {
 	int i;
 
 	i = 0;
 
-	while(src != '\0')
-		{
-			dst[i] = src[i];
-			i++;
-		}
-	dst[1] = src[1];
-
-	return(*dst);
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (src[i] == '\0')
+	{
+		dst[i] = src[i];
+	}
+	return (dst);
 }
