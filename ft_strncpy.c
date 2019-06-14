0a28ccbed5d500e5a7026c90ee244825134c1868
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-14 14:03:59 +0200

    few changes need to be made

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,27 +6,34 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 10:29:00 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/05/31 06:59:40 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/14 14:03:35 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strncpy(char *dst, const char *src, size_t len)
+char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
 	size_t i;
 
 	i = 0;
 
-	while (src != '\0')
+	if (len <= 0)
+		return (NULL);
+	if (i  ==  len)
+	{
+		dst[i] = src[i];
+	}	
+		while (i < len)
 		{
-			dst[len] = src[i];
+			dst[i] = src[i];
 			i++;
 		}
-	if (i > len)
-		{
-			dst[len] = '\0';
-			len++;
-		}
+		if (len == '\0')
+			while (i < len)
+			{
+				dst[i] = '\0';
+				i++;
+			}
 	return(dst);
 }
