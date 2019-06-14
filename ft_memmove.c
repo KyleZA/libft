56762b8342cfbb8bf1d756b0c2b4f0c376a3b632
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-14 09:17:00 +0200

    changes needs to be made with the code

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 13:39:05 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/05 13:47:35 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/14 09:16:31 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,30 @@
 
 void *ft_memmove(void *dst, const void *src, size_t len)
 {
-	int i;
-
-	i = 0;
-
-	unsigned char *chrdst;
-	unsigned char *chrsrc;
-
-	chrdst = (unsigned char *)dst;
-	chrsrc = (unsigned char *)src;
+	
+	size_t i;
+	char *chrdst;
+	char *chrsrc;
 
 	if (dst == NULL && src == NULL)
-	{
 		return (NULL);
-	}
-	while ((len > 0) && chrsrc[i] != '\0')
-	{
-		i++;
-		len--;
-	}
-	while (len > 0)
-	{
-		chrdst[i] = chrsrc[i];
-		i++;
-		len--;
-	}
+
+	chrdst = (char *)dst;
+	chrsrc = (char *)src;
+	i = len;
+	
+	if (dst > src)
+		while (i > 0)
+		{
+			chrdst[i] = chrsrc[i];
+			i--;
+		}
+	else
+		while (len > 0)
+		{
+			chrdst[i] = chrsrc[i];
+			i++;
+			len--;
+		}
 	return (dst);
 }
