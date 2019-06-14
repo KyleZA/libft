e6079ae3791abd1b4ac0c0ee72bd10c05a9927d5
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-14 18:52:07 +0200

    completed memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 13:39:05 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/14 09:16:31 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/14 18:51:53 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,30 @@
 
 void *ft_memmove(void *dst, const void *src, size_t len)
 {
-	
-	size_t i;
+	int i;
+	int j;
+	int k;
 	char *chrdst;
 	char *chrsrc;
 
 	if (dst == NULL && src == NULL)
 		return (NULL);
-
 	chrdst = (char *)dst;
 	chrsrc = (char *)src;
 	i = len;
-	
+	j = 0;
+	k = 0;
+
 	if (dst > src)
-		while (i > 0)
-		{
-			chrdst[i] = chrsrc[i];
-			i--;
-		}
+		while (i-- > 0)
+		chrdst[i] = chrsrc[i];
 	else
 		while (len > 0)
-		{
-			chrdst[i] = chrsrc[i];
-			i++;
-			len--;
-		}
+	{
+		chrdst[j] = chrsrc[k];
+		j++;
+		k++;
+		len--;
+	}
 	return (dst);
 }
