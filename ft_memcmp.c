0ac3fd7b73791065a99ee9633142ef998511b25b
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-15 02:16:58 +0200

    commiting completed memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 10:16:45 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/06 11:24:46 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/15 02:16:40 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,20 @@
 
 int ft_memcmp(const void *s1, const void *s2, size_t n)
 {	
+
+	unsigned char *chrs1;
+	unsigned char *chrs2;
 	
-	int i;
+	chrs1 = (unsigned char *)s1;
+	chrs2 = (unsigned char *)s2;
 
-	i = 0;
-
-	char *chrs1;
-	char *chrs2;
-	
-	chrs1 = (char *)s1;
-	chrs2 = (char *)s2;
-
-	if (chrs1[i] == chrs2[i])
+	while (n > 0)
 	{
-		return (0);
-	}
-	while  ((n > 0) && (chrs1[n] < chrs2[n]))
-	{
-		i++;
+		if (*chrs1 != *chrs2)
+			return (*chrs1 - *chrs2);
 		n--;
-		return (chrs2 - chrs1);
-	}
-	while ((n > 0) && (chrs2[i] < chrs2[i]))
-	{
-		i++;
-		n--;
-		return (chrs1 - chrs2);
+		chrs1++;
+		chrs2++;
 	}
 	return (0);
 }
