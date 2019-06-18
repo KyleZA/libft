e5812ff29b1c16425ae3ac4612d3e0cba47ce149
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-18 09:54:54 +0200

    commiting completed strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 15:54:18 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/05/28 16:15:02 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/18 09:54:29 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,13 @@
 char *ft_strchr(const char *s, int c)
 {
 	unsigned char charr;
+	int i;
 
-	charr = c;
-
-	while (s)
-		if(*s == charr)
-		{
-			return ((char*)s);
-			s++;
-		}
-	return ('\0');
+	charr = (unsigned char)c;
+	i = 0;
+	while (*s && *s != c)
+		s++;
+	if(*s == charr)
+		return ((char*)s + i);			
+	return (NULL);
 }
