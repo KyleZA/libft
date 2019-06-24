5f792644d527845d0c4e7fef86d89d0ede502fe9
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-24 13:50:51 +0200

    committing completed strmap

---
+++ ft_strmap.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/24 11:35:09 by kfrancis          #+#    #+#             */
+/*   Updated: 2019/06/24 12:07:02 by kfrancis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	char *newstr;
+	size_t i;
+
+	if (s == NULL || f == NULL)
+		return (NULL);
+	newstr = ft_strnew(ft_strlen(s));
+		if (newstr == NULL)
+			return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		newstr[i] = (*f)(s[i]);
+		i++;
+	}
+	return (newstr);
+}
