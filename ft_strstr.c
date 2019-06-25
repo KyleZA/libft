16d9fd66f77665c06786658fb37f645e5e247f37
Kyle Francis <kfrancis@c3r6s2.wethinkcode.co.za>
2019-06-25 16:44:04 +0200

    pasts test but might need to double check

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 09:41:25 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/24 10:41:38 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/25 16:42:44 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,20 @@ char *ft_strstr(const char *haystack, const char *needle)
 	int j;
 
 	i = 0;
-	j = 0;
 
 	if (needle[i] == '\0')
-		return ((char *)haystack);
-	while (haystack[i] != needle[i])
+		return ((char*)haystack);
+	while (haystack[i] != '\0')
 	{
+		if (haystack[i] == needle[0])
+		{
+			j = 0;;
+			while (needle[j] != '\0' && haystack[i + j] == needle[j])
+				j++;
+			if (needle[j] == '\0')
+				return ((char*)&haystack[i]);
+		}
 		i++;
-	}
-	if (haystack[i] == needle[j + 1])
-	{
-		while (haystack[i] != '\0' && needle[j] != '\0')
-		return ((char *)&haystack[i]);
-			i++;
-			j++;
 	}
 	return (0);
 }
