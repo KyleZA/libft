9a427529ee62da8c9cc05c91ffd156a49c28d105
Kyle Francis <kfrancis@c3r6s2.wethinkcode.co.za>
2019-06-25 16:45:22 +0200

    protected the function

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 09:01:58 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/19 09:06:13 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/25 16:45:11 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int ft_strequ(char const *s1, char const *s2)
 {
+	if (!s1)
+		return (0);
 	if (ft_strlen(s1) == ft_strlen(s2))
 		return (1);
 	else
