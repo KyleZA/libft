c7f6451a353c0ae1dcd934882736e22442b54107
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-26 16:28:06 +0200

    completed memcmp and norm fixes

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,21 +6,19 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 10:16:45 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/15 02:16:40 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/26 16:27:45 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_memcmp(const void *s1, const void *s2, size_t n)
-{	
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*chrs1;
+	unsigned char	*chrs2;
 
-	unsigned char *chrs1;
-	unsigned char *chrs2;
-	
 	chrs1 = (unsigned char *)s1;
 	chrs2 = (unsigned char *)s2;
-
 	while (n > 0)
 	{
 		if (*chrs1 != *chrs2)
