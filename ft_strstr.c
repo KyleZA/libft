a6ce08871d4f433c578b08b259fb04a7a6838541
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-26 16:46:19 +0200

    completed strstr and norm fixes

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,27 +6,26 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 09:41:25 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/25 16:42:44 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/26 16:45:59 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strstr(const char *haystack, const char *needle)
+char	*ft_strstr(const char *haystack, const char *needle)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = 0;
-
 	if (needle[i] == '\0')
 		return ((char*)haystack);
 	while (haystack[i] != '\0')
 	{
 		if (haystack[i] == needle[0])
 		{
-			j = 0;;
-			while (needle[j] != '\0' && haystack[i + j] == needle[j])
+			j = 0;
+			while ((needle[j] != '\0') && (haystack[i + j] == needle[j]))
 				j++;
 			if (needle[j] == '\0')
 				return ((char*)&haystack[i]);
