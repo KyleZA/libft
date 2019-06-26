ee4923b92938d608ec8a4f39cfc3a3fbc102db46
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-26 16:18:38 +0200

    cmopleted memmove fixed norm fixes

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,19 +6,19 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 13:39:05 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/14 18:51:53 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/26 16:18:15 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	int i;
-	int j;
-	int k;
-	char *chrdst;
-	char *chrsrc;
+	int		i;
+	int		j;
+	int		k;
+	char	*chrdst;
+	char	*chrsrc;
 
 	if (dst == NULL && src == NULL)
 		return (NULL);
@@ -27,17 +27,16 @@ void *ft_memmove(void *dst, const void *src, size_t len)
 	i = len;
 	j = 0;
 	k = 0;
-
 	if (dst > src)
 		while (i-- > 0)
-		chrdst[i] = chrsrc[i];
+			chrdst[i] = chrsrc[i];
 	else
 		while (len > 0)
-	{
-		chrdst[j] = chrsrc[k];
-		j++;
-		k++;
-		len--;
-	}
+		{
+			chrdst[k] = chrsrc[j];
+			j++;
+			k++;
+			len--;
+		}
 	return (dst);
 }
