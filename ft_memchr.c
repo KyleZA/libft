61590a9f9b1b23dbf8ff8ca7011fe9e4dcca85bb
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-26 16:25:20 +0200

    pushing completed memchr and norm fixes

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,26 +6,26 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 13:48:33 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/15 01:59:08 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/26 16:25:00 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t i;
-	
-	i = 0;	
-	unsigned char *str;
+	size_t			i;
+	unsigned char	*str;
+
+	i = 0;
 	str = (unsigned char *)s;
-		while (i < n)
-		{	
-			if (str[i] == (unsigned char)c)
-			{
-				return (&str[i]);
-			}
-			i++;
+	while (i < n)
+	{
+		if (str[i] == (unsigned char)c)
+		{
+			return (&str[i]);
 		}
-		return (NULL);
+		i++;
+	}
+	return (NULL);
 }
