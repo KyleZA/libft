a78c9c7f5a14002962d344f32b8c6df9b877d36c
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-26 16:44:27 +0200

    completed strrchr and norm fixes

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,21 +6,19 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 10:19:39 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/07 10:44:52 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/26 16:44:11 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strrchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
-	int i;
-
-	unsigned char chr;
+	int				i;
+	unsigned char	chr;
 
 	chr = (unsigned char)c;
 	i = ft_strlen(s);
-
 	while ((i > 0) && (s[i] != chr))
 	{
 		i--;
