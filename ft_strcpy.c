1b7c7aed32e89f83f786822d733840e91357e1d8
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-26 16:32:54 +0200

    completed strcpy and norm fixes

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,24 +6,23 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 10:18:07 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/14 12:21:57 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/26 16:32:34 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strcpy(char *dst, const char *src)
+char	*ft_strcpy(char *dst, const char *src)
 {
-	int i;
+	int	i;
 
 	i = 0;
-
 	while (src[i] != '\0')
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	if(src[i] == '\0')
+	if (src[i] == '\0')
 	{
 		dst[i] = src[i];
 	}
