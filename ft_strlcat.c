8c588a72f669eb6f96188135ef68c4e43fbfce33
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-26 16:38:47 +0200

    completed strclat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,20 +6,19 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 06:58:28 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/11 09:40:36 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/26 16:38:32 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 {
-	size_t i;
-	size_t j;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
 	j = 0;
-
 	while (dst[j] && j < dstsize)
 		j++;
 	i = j;
@@ -29,6 +28,6 @@ size_t ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
 		j++;
 	}
 	if (i < dstsize)
-	dst[j] = '\0';
+		dst[j] = '\0';
 	return (i + ft_strlen(src));
 }
