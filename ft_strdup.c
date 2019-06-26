63c60fb0608ae51b7cc2249193bb93b0ddcde2b7
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-26 16:31:29 +0200

    completed strdup and norm fixes

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 15:02:28 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/05/31 09:56:35 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/26 16:31:10 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,18 @@
 
 char	*ft_strdup(const char *s1)
 {
-	char *dst;
-
-	int i;
+	char	*dst;
+	int		i;
 
 	i = 0;
-
 	dst = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
 	if (dst == NULL)
 		return (NULL);
-
-		while (s1[i] != '\0')
-		{
-			dst[i] = s1[i];
-			i++;
-		}
-		dst[i] = '\0';
-		return (dst);
+	while (s1[i] != '\0')
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
 }
