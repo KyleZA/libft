d7c0746eadcfaeb37ec3429464980793d15b3a62
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-27 09:05:22 +0200

    completed strcmp and norm fixes

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,23 +6,21 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 14:56:19 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/18 11:22:04 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/27 09:04:59 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_strcmp(const char *s1, const char *s2)
+int	ft_strcmp(const char *s1, const char *s2)
 {
-	int i;
+	int				i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
 	i = 0;
-
-	unsigned char *str1;
-	unsigned char *str2;
-
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-
 	if (str1[i] && str2[i])
-		while((str1[i] != '\0' && str2[i] != '\0') && (str1[i] == str2[i]))
+		while ((str1[i] != '\0' && str2[i] != '\0') && (str1[i] == str2[i]))
 		{
 			i++;
 		}
@@ -30,5 +28,5 @@ int ft_strcmp(const char *s1, const char *s2)
 		return (1);
 	else if (str1[i] < str2[i])
 		return (-1);
-	return(0);
+	return (0);
 }
