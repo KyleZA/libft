c6d4b11c79b7d77a93c9fec79aaab86e4b9cc94c
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-27 09:06:41 +0200

    pushing  completed strncmp and norm fixes

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,22 +6,20 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 11:42:06 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/24 16:46:58 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/27 09:06:18 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t i;
-
-	unsigned char *str1;
-	unsigned char *str2;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-
 	i = 0;
 	while (i < n)
 	{
