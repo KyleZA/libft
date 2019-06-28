bef42be31687a2aa9646b91ddc3fda4c318c0239
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-06-28 17:00:35 +0200

    need changes to be made to function

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,26 +6,36 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 09:58:18 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/05/31 06:58:53 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/06/28 17:00:12 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
+
+#include "libft.h"
 
 int ft_atoi(const char *str)
 {
+	unsigned ret;
+	int len;
 	int i;
-	
- 	i = 0;
-	
-	while(str[i] != '\0')
+	int j;
+	int sign;
+
+	sign = 1;
+	ret = 0;
+	len = 0;
+	i = 0;
+	j = -1;
+	while (ft_isspace(*str))
+		str++;
+	len = (ft_strlen(str));
+	while (++j < len)
 	{
-			if(str[i] >= '0' && str[i] <= '9')
-			{	
-				i++;
-				return(*str);
-			}
+		if (ft_isdigit(str[j]))
+			ret = (ret * 10) + (str[j] - '0');
+		else if (j == 0 && str[j]== '-')
+			sign = -1;
+		else if (str[j] == '+')
+			sign = 1;
 	}
-	return (0);
+	return (ret * sign);
 }
