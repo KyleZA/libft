38515ca55dd5e155337e00af4a665b2cfd71f4ac
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-07-02 14:16:59 +0200

    completed alnum and norm fixes

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,13 +6,13 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 15:28:15 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/03 15:36:32 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/07/02 14:16:20 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isalnum(int c)
+int		ft_isalnum(int c)
 {
 	if (ft_isalpha(c) == 1 || ft_isdigit(c) == 1)
 		return (1);
