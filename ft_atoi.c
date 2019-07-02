dbd7338d9526f68798bde0f93e106fdbe05384c6
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-07-02 14:14:59 +0200

    completed atoi and norm fixes

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,13 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 09:58:18 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/07/02 12:57:33 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/07/02 14:14:18 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_atoi(const char *str)
+int		ft_atoi(const char *str)
 {
 	int i;
 	int sign;
@@ -23,7 +23,7 @@ int ft_atoi(const char *str)
 		return (0);
 	while (ft_isspace(str[i]))
 		i++;
-	sign = 1;	
+	sign = 1;
 	if ((str[i] == '-') || (str[i] == '+'))
 	{
 		if (str[i] == '-')
@@ -36,5 +36,5 @@ int ft_atoi(const char *str)
 		val = (val * 10) + (str[i] - '0');
 		i++;
 	}
-	return (sign  * val);
+	return (sign * val);
 }
