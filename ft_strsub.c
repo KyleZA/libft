43ff54b5262a052437883939beabb64ca8503289
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-07-04 13:35:09 +0200

    completed strsub and norm fixes

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,28 +6,28 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/24 11:42:35 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/24 12:29:14 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/07/04 13:34:49 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strsub(char const *s, unsigned int start, size_t len)
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
-	char *newstr;
-	size_t i;
-	
+	char	*newstr;
+	size_t	i;
+
 	i = 0;
 	if (s == NULL)
 		return (NULL);
 	newstr = (char *)malloc(sizeof(char) * len + 1);
 	if (newstr == NULL)
-	   return (NULL);
+		return (NULL);
 	while (i < len)
 	{
 		newstr[i] = s[i + start];
 		i++;
 	}
-	newstr[i]= '\0';
+	newstr[i] = '\0';
 	return (newstr);
 }
