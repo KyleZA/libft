9f6d55e54243136885b2ed1828081f1cbf93ba36
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-07-04 09:15:50 +0200

    completed isdigit and norm fixes

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,15 +6,15 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 15:24:52 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/03 15:28:01 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/07/04 09:15:20 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isdigit(int c)
+int		ft_isdigit(int c)
 {
-	if(c >= '0' && c <= '9')
+	if (c >= '0' && c <= '9')
 		return (1);
 	return (0);
 }
