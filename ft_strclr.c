b1500e110f28ac3617a85b3be0a7a47488339e3b
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-07-04 09:51:52 +0200

    completed strclr and norm fixes

--- ft_strclr.c
+++ ft_strclr.c
@@ -6,18 +6,17 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/14 14:09:40 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/14 14:21:15 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/07/04 09:51:21 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_strclr(char *s)
+void	ft_strclr(char *s)
 {
 	int i;
 
 	i = 0;
-
 	if (!s)
 		return ;
 	while (s[i] != '\0')
