317f73d5274eccf4d78021cb1ddfdacde2073a65
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-07-04 09:54:41 +0200

    completed strequ and norm fixes

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,13 +6,13 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 09:01:58 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/25 16:45:11 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/07/04 09:54:20 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_strequ(char const *s1, char const *s2)
+int	ft_strequ(char const *s1, char const *s2)
 {
 	if (!s1)
 		return (0);
@@ -20,5 +20,4 @@ int ft_strequ(char const *s1, char const *s2)
 		return (1);
 	else
 		return (0);
-
 }
