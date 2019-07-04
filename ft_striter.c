6288e2271913e91f830bc873ee4360cc1bc4bd2e
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-07-04 09:55:33 +0200

    completed striter and norm fixes

--- ft_striter.c
+++ ft_striter.c
@@ -6,13 +6,13 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 14:58:22 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/19 15:04:11 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/07/04 09:55:14 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_striter(char *s, void (*f)(char*))
+void	ft_striter(char *s, void (*f)(char*))
 {
 	int i;
 
