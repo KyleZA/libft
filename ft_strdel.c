ddd39091985c71de56f6ebc0e1cd7e90b6c7c317
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-07-04 09:53:29 +0200

    completed strdel with norm fixes

--- ft_strdel.c
+++ ft_strdel.c
@@ -6,18 +6,17 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 12:39:19 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/10 12:41:33 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/07/04 09:53:12 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void 	ft_strdel(char **as)
+void	ft_strdel(char **as)
 {
 	if (as != NULL)
 	{
 		free(*as);
 		*as = NULL;
 	}
-
 }
