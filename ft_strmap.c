deea88513566978e0afd77268ff85231b3dc4057
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-07-04 13:05:37 +0200

    completed strmap and norm fixes

--- ft_strmap.c
+++ ft_strmap.c
@@ -6,7 +6,7 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/24 11:35:09 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/24 12:07:02 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/07/04 13:05:05 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 char	*ft_strmap(char const *s, char (*f)(char))
 {
-	char *newstr;
-	size_t i;
+	char	*newstr;
+	size_t	i;
 
 	if (s == NULL || f == NULL)
 		return (NULL);
 	newstr = ft_strnew(ft_strlen(s));
-		if (newstr == NULL)
-			return (NULL);
+	if (newstr == NULL)
+		return (NULL);
 	i = 0;
 	while (s[i] != '\0')
 	{
