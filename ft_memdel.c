157d62d9a692ab060c3e29120ae0a48957d1a5d5
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-07-04 09:30:01 +0200

    completed memdel and norm fixes

--- ft_memdel.c
+++ ft_memdel.c
@@ -6,13 +6,13 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 13:15:01 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/19 13:32:21 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/07/04 09:29:38 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_memdel(void **ap)
+void	ft_memdel(void **ap)
 {
 	if (ap != NULL)
 	{
