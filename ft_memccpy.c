91c496c8a2f8e5c47f4d36b53d4c936826892fc2
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-07-04 09:29:03 +0200

    completed memccpy and norm fixes

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,31 +6,28 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 13:32:32 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/14 17:02:20 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/07/04 09:28:11 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memccpy(void *dst, const void *src, int c, size_t n)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	size_t i;
+	size_t			i;
+	unsigned char	*chrdst;
+	unsigned char	*chrsrc;
 
 	i = 0;
-
-	unsigned char *chrdst;
-	unsigned char *chrsrc;
-
 	chrdst = (unsigned char *)dst;
 	chrsrc = (unsigned char *)src;
-
 	while (n > 0)
 	{
 		chrdst[i] = chrsrc[i];
 		if (chrsrc[i] == (unsigned char)c)
 			return (chrdst + i + 1);
-	i++;
-	n--;
+		i++;
+		n--;
 	}
 	return (NULL);
 }
