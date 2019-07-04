ba159f31c9773f8247249893688fc9fb42aac7f8
Kyle Francis <kfrancis@c3r8s4.wethinkcode.co.za>
2019-07-04 09:44:55 +0200

    completed putnbr_fd and norm fixes

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,13 +6,13 @@
 /*   By: kfrancis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 15:24:31 by kfrancis          #+#    #+#             */
-/*   Updated: 2019/06/20 15:54:31 by kfrancis         ###   ########.fr       */
+/*   Updated: 2019/07/04 09:44:30 by kfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
 		write(fd, "-2147483648", 11);
@@ -21,7 +21,7 @@ void ft_putnbr_fd(int n, int fd)
 		ft_putchar_fd('-', fd);
 		ft_putnbr_fd(-n, fd);
 	}
-	else if(n > 9)
+	else if (n > 9)
 	{
 		ft_putnbr_fd(n / 10, fd);
 		ft_putchar_fd(n % 10 + '0', fd);
